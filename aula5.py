977315daa8be258990e9352b118341eb2d853c21
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-06-04 13:20:04 -0300

    feat: Added aula5(bool type and logic operator '==' in python).

---
+++ aula5.py
@@ -0,0 +1,15 @@
+# Tipo de dado bool (boolean)
+
+# Ao questionar algo em um programa, 
+# só existem duas respostas possíveis: 
+# sim (True) ou não (False).
+
+# Existem vários operadores para "questionar". 
+# Dentre eles, o ==, que é um operador lógico que 
+# questiona se um valor é igual a outro.
+
+print(10==10) #Sim = True(verdadeiro)
+print('oi'=='oi') #Sim = True(verdadeiro)
+print(11==-11) #Não = False(falso)
+print('falou'=='tchau') #Não = False(falso)
+print(type('oi'=='olá')) #Mostra(print) o tipo(type) de retorno da expressão(bool)
