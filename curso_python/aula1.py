5e161f2598a6752da092aa71b450fe7159b2d5d4
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-06-04 12:30:25 -0300

    chore: initial project setup. feat: added the first leasson: comments, docstrings and print function.

---
+++ curso_python/aula1.py
@@ -0,0 +1,22 @@
+# Comentário in-line
+
+#Função imprimir
+print('Hello, world!')
+#Outro exemplo
+print(123)
+
+
+"""
+
+DocString
+O Python lê
+
+"""
+
+#Outro exemplo de DocString
+'''
+
+Geralmente usado para explicar o código
+mais detalhadamente.
+
+'''
